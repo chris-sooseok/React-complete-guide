2d6402dc82650e327a924efe381ff19068c8b1ec
chris-sooseok <sooseokkim99@gmail.com>
2025-06-04 15:11:27 +0900

    Forwarding props to wrapped elements
    - helps us propagate all elements to be passed to component all together

---
+++ 01-starting-project/src/components/Examples.jsx
@@ -0,0 +1,64 @@
+import TabButton from "./TabButton";
+import { EXAMPLES } from "../data-with-examples";
+import { useState } from "react";
+import Section from "./Section";
+
+export default function Examples() {
+  /* Hooks: useState
+    Only call hooks inside of component function
+    Only call hooks on the top level */
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContant = <p>Please select a topic</p>;
+
+  if (selectedTopic) {
+    tabContant = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  return (
+    <Section title="Examples" id="examples">
+      <menu>
+        <TabButton
+          isSelected={selectedTopic === "components"}
+          onClick={function () {
+            handleSelect("components");
+          }}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onClick={() => handleSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onClick={() => handleSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onClick={() => handleSelect("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+      {tabContant}
+    </Section>
+  );
+}
--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,11 +1,12 @@
-export default function TabButton({children, onSelect, isSelected}) {
+export default function TabButton({ children, onSelect, isSelected }) {
+  // ? in js, this would be a general approach to handle this action
+  // document.querySelector('button').addEventListener('click', () => {})
 
-    // ? in js, this would be a general approach to handle this action
-    // document.querySelector('button').addEventListener('click', () => {})
-    
   return (
     <li>
-      <button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
     </li>
   );
 }
