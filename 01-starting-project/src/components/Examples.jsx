eee777a77bae8025d7d1679db179374454f790f5
chris-sooseok <sooseokkim99@gmail.com>
2025-06-04 15:46:39 +0900

    tic-tac-toe project starting

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -32,7 +32,6 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
-        buttonsContainer="menu"
         buttons={
           <>
             <TabButton
--- 01-starting-project/src/components/Tabs.jsx
+++ 01-starting-project/src/components/Tabs.jsx
@@ -1,4 +1,4 @@
-export default function Tabs({ children, buttons, buttonsContainer }) {
+export default function Tabs({ children, buttons, buttonsContainer="menu"}) {
     // depending on the value passed (ex. "menu" or custom componenet), it will load appropriately
     const ButtonsContainer = buttonsContainer;
     
